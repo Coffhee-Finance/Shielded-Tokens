f0eb790031b4a807b2d977b292bb6139e5c8ecde
Lamont <missionpossible23@protonmail.com>
2024-12-01 20:51:39 -0500

    Update Cusde.sol

--- cUSD.e/contracts/Cusde.sol
+++ cUSD.e/contracts/Cusde.sol
@@ -1,1 +1,52 @@
-//Token Contract
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@fhenixprotocol/contracts/FHE.sol";
+import "@layerzerolabs/oft-evm/contracts/OFT.sol";
+
+
+contract cUsde is ERC20 {
+
+    mapping(address => euint32) internal _encBalances;
+
+    constructor(string memory name, string memory symbol) ERC20("Confidential USD.e", "cUSDe") {
+        address _lzEndpoint;
+        address _delegate;
+        _mint(msg.sender, 100 * 10 ** uint(decimals()));
+    }
+
+    function wrap(uint32 amount) public {
+        // Make sure that the sender has enough of the public balance
+        require(balanceOf(msg.sender) >= amount);
+        // Burn public balance
+        _burn(msg.sender, amount);
+
+        // convert public amount to shielded by encrypting it
+        euint32 shieldedAmount = FHE.asEuint32(amount);
+        // Add shielded balance to his current balance
+        _encBalances[msg.sender] = _encBalances[msg.sender] + shieldedAmount;
+    }
+
+    function unwrap(inEuint32 memory amount) public {
+        euint32 _amount = FHE.asEuint32(amount);
+        // verify that our shielded balance is greater or equal than the requested amount 
+        FHE.req(_encBalances[msg.sender].gte(_amount));
+        // subtract amount from shielded balance
+        _encBalances[msg.sender] = _encBalances[msg.sender] - _amount;
+        // add amount to caller's public balance by calling the `mint` function
+        _mint(msg.sender, FHE.decrypt(_amount));
+    }
+
+    function transferEncrypted(address to, inEuint32 calldata encryptedAmount) public {
+        euint32 amount = FHE.asEuint32(encryptedAmount);
+        // Make sure the sender has enough tokens.
+        FHE.req(amount.lte(_encBalances[msg.sender]));
+
+        // Add to the balance of `to` and subract from the balance of `from`.
+        _encBalances[to] = _encBalances[to] + amount;
+        _encBalances[msg.sender] = _encBalances[msg.sender] - amount;
+    }
+    
+}
