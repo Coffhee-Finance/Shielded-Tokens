a904bdf72329afccafcc8b10571914e8a99b29ee
Lamont <missionpossible23@protonmail.com>
2025-05-15 09:20:32 -0400

    Update CappuccinoToken.sol

--- CappuccinoTokens/contracts/CappuccinoToken.sol
+++ CappuccinoTokens/contracts/CappuccinoToken.sol
@@ -58,33 +58,24 @@ contract CappuccinoToken is ERC20Upgradeable, TokenRouter {
     }
 
     function wrap(uint256 amount) public {
-        // Make sure that the sender has enough of the public balance
         require(balanceOf(msg.sender) >= amount);
-        // Burn public balance
         _burn(msg.sender, amount);
 
-        // convert public amount to shielded by encrypting it
         euint32 shieldedAmount = FHE.asEuint32(amount);
-        // Add shielded balance to his current balance
         _encBalances[msg.sender] = _encBalances[msg.sender] + shieldedAmount;
     }
 
     function unwrap(inEuint32 memory amount) public {
-        euint32 _amount = FHE.asEuint32(amount);
-        // verify that our shielded balance is greater or equal than the requested amount 
+        euint32 _amount = FHE.asEuint32(amount); 
         FHE.req(_encBalances[msg.sender].gte(_amount));
-        // subtract amount from shielded balance
         _encBalances[msg.sender] = _encBalances[msg.sender] - _amount;
-        // add amount to caller's public balance by calling the `mint` function
         _mint(msg.sender, FHE.decrypt(_amount));
     }
 
     function transferEncrypted(address to, inEuint32 calldata encryptedAmount) public {
         euint32 amount = FHE.asEuint32(encryptedAmount);
-        // Make sure the sender has enough tokens.
         FHE.req(amount.lte(_encBalances[msg.sender]));
 
-        // Add to the balance of `to` and subract from the balance of `from`.
         _encBalances[to] = _encBalances[to] + amount;
         _encBalances[msg.sender] = _encBalances[msg.sender] - amount;
     }
